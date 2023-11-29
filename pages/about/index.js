3b18539a49338e6e4cbd1670604abf30969c3a4b
Ivan Lozic <ivanlozic995@gmail.com>
2023-11-28 21:38:53 -0500

    about page finished

--- pages/about/index.js
+++ pages/about/index.js
@@ -93,75 +93,89 @@ export const aboutData = [
   }
 ]
 
-import Avatar from '../../components/Avatar'
-import Circles from '../../components/Circles'
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
 
-import { motion } from 'framer-motion'
-import { fadeIn } from '../../variants'
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
+import CountUp from 'react-countup';
 
 const About = () => {
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
+
   return (
-    <div className='h-full bg-primary/30 py-32 text-center xl:text-left'>
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
       <motion.div
         variants={fadeIn('right', 0.2)}
-        initial='hidden'
-        animate='show'
-        exit='hidden'
-        className='hidden xl:flex absolute bottom-0 -left-[370px]'
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
       >
         <Avatar />
       </motion.div>
-      <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
-        <div className='flex-1 flex flex-col justify-center'>
-          <h2>
-            Captivating <span>stories</span> birth magnificent designs
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
+        <div className="flex-1 flex flex-col justify-center">
+          <h2 className="text-4xl">
+            Captivating <span className="text-accent">stories</span> birth magnificent designs
           </h2>
-        </div>
-        <div className='flex flex-col w-full xl:max-w-[48%]h-[480px]'>
-          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
-            {aboutData.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className={`${
-                    index === itemIndex &&
-                    'text-accent after:w-[100%] after:bg-accent after:transition-all'
-                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 `}
-                  onClick={() => setIndex(itemIndex)}
-                >
-                  {item.title}
-                </div>
-              )
-            })}
+          <p className="max-w-[500px] mx-auto xl:mx-0 mb-6 xl:mb-12 px-2 xl:px-0">
+            3 years ago, I began freelancing as a developer. Since then, I have done remote work for agencies, consulted for startups, and collaborated on digital products for business and consumer use
+          </p>
+
+          <div className="flex flex-1 xl:gap-x-6">
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <CountUp start={0} end={3} duration={3} />
+            </div>
+            <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Years of experience</div>
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <CountUp start={0} end={3} duration={3} />
+            </div>
+            <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Clients</div>
+            <div className="relative flex-1 after:w-[1px] after:h-full after:bg-white/10 after:absolute after:top-0 after:right-0">
+              <CountUp start={0} end={3} duration={3} />
+            </div>
+            <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">Finished projects</div>
           </div>
-          <div className='bg-pink-400/10 py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
-            {aboutData[index].info.map((item, itemIndex) => {
-              return (
-                <div
-                  key={itemIndex}
-                  className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
-                >
-                  <div className='font-light mb-2 md:mb-0'>{item.title}</div>
-                  <div className='hidden md:flex'></div>
-                  <div>{item.stage}</div>
-                  <div className='felx gap-x-4'>-</div>
-                  {item.icons?.map((icon, itemIndex) => {
-                    return (
-                      <div key={itemIndex} className='text-2xl text-white'>
-                        {icon}
-                      </div>
-                    )
-                  })}
-                </div>
-              )
-            })}
+        </div>
+
+        <div className="flex flex-col w-full xl:max-w-[48%] h-[480px]">
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => (
+              <div
+                key={itemIndex}
+                className={`${
+                  index === itemIndex &&
+                  'text-accent after:w-[100%] after:bg-accent after:transition-all'
+                } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 `}
+                onClick={() => setIndex(itemIndex)}
+              >
+                {item.title}
+              </div>
+            ))}
+          </div>
+
+          <div className="bg-pink-400/10 py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start">
+            {aboutData[index].info.map((item, itemIndex) => (
+              <div key={itemIndex} className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60">
+                <div className="font-light mb-2 md:mb-0">{item.title}</div>
+                <div className="hidden md:flex"></div>
+                <div>{item.stage}</div>
+                <div className="flex gap-x-4">-</div>
+                {item.icons?.map((icon, iconIndex) => (
+                  <div key={iconIndex} className="text-2xl text-white">
+                    {icon}
+                  </div>
+                ))}
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
